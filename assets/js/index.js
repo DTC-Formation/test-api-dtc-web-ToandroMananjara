c97dfb63d1309de9c5243a1abf6e9b54c3c597f7
ToandroMananjara <mahasalotra@gmail.com>
2023-07-31 11:32:08 +0000

    logique avec les boutons voir plus rectifié pour agir independemment

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,22 +6,22 @@ let liPhone
 let liWebsite
 let liCompany
 let img
-let btn
+let btn 
 
+
+// la fonction qui cree tous les contenu html 
 function create(){
-    let containerItem=document.createElement('div')
+    let containerItem=document.createElement('div') // creation div container-item
     containerItem.classList.add('container-item')
     document.querySelector('body').appendChild(containerItem)
     img=document.createElement('img')
     
-    let imgContainer=document.createElement('div')
+    let imgContainer=document.createElement('div') // creation div img-container
     imgContainer.classList.add('img-container')
     imgContainer.appendChild(img)
     
-    let about = document.createElement('div')
+    let about = document.createElement('div')      // creation div about
     about.classList.add('about')
-    
-    h1 = document.createElement('h1')
     
     containerItem.append(imgContainer,about)
     
@@ -36,13 +36,13 @@ function create(){
     liEmail = document.createElement('li')
     liEmail.classList.add('email')
     liAdress = document.createElement('li')
-    liAdress.classList.add('address')
+
     liPhone = document.createElement('li')
-    liPhone.classList.add('phone')
+    
     liWebsite = document.createElement('li')
-    liWebsite.classList.add('Website')
+    
     liCompany = document.createElement('li')
-    liCompany.classList.add('Company')
+    
     ul.append(liName,liUserName,liEmail,liAdress,liPhone,liWebsite,liCompany)
 
     btn = document.createElement("button")
@@ -50,49 +50,106 @@ function create(){
     containerItem.appendChild(btn)
     
 }
+function displayNone(display){
+    display.style.display="none"
+}
 
+function displayBlock(display){
+    display.style.display="block"
+}
+
+let bt = []         // on va recuperer tous les boutons dans le tableau bt 
+let address = []    // on va recuperer tous les li address dans le tableau bt
+let phone = []      // on va recuperer tous les li phone dans le tableau bt
+let website = []    // on va recuperer tous les li website dans le tableau bt
+let company = []    // on va recuperer tous les li company dans le tableau bt
 setTimeout(() => {
-    let loader = document.querySelector('.loader').remove()
-    fetch("./assets/user.json")
-.then(res => res.json())
-.then(user => {
-    for (let k = 0 ; k<user.length;k++){
-        let obj = user [k] 
-        
-        create()
-        btn.addEventListener('click',function(){
-            document.querySelectorAll('button').forEach(element4=>{
-                element4.title="afficher plus de resultat"
-                if(element4.innerHTML=='<i class="fa-regular fa-eye"></i>' ){
-                    element4.innerHTML='<i class="fa-regular fa-eye-slash"></i>'
-                }
-                else if(element4.innerHTML=='<i class="fa-regular fa-eye-slash"></i>' ){
-                    element4.innerHTML='<i class="fa-regular fa-eye"></i>'
-                }
-                  
-            })          
-            document.querySelectorAll('.address').forEach(element=>{
-                element.classList.toggle('toggle')
-            })
-            document.querySelectorAll('.Website').forEach(element2=>{
-                element2.classList.toggle('toggle')
-            })
-            document.querySelectorAll('.phone').forEach(element2=>{
-                element2.classList.toggle('toggle')
-            })
-            document.querySelectorAll('.Company').forEach(element3=>{
-                element3.classList.toggle('toggle')
-            })
-        })           
-        img.src = "./assets/img/user-avatar.png"
-        liName.innerHTML =    '<span class="ok">Name</span>'+'<span>: </span>'+ obj.name
-        liUserName.innerHTML ='<span class="ok">UserName</span> '+'<span>: </span>' +obj.username
-        liEmail.innerHTML =   '<span class="ok">Email</span>'+'<span>: </span>' +  obj.email
-        liAdress.innerHTML =  '<span class="ok">Address</span>'+'<span>: </span>' + obj.address
-        liPhone.innerHTML =   '<span class="ok">Phone</span>'+'<span>: </span>' + obj.phone
-        liWebsite.innerHTML = '<span class="ok">Website</span>'+'<span>: </span>' + obj.website
-        liCompany.innerHTML = '<span class="ok">Company</span>'+'<span>: </span>' + obj.company
-    }
-});
     
-}, 5000);
+    fetch("./assets/user.json")
+    .then(res => res.json())
+    .then(user => {
+        for (let k = 0 ; k<user.length;k++){
+            let obj = user [k] 
+            
+            create()
+
+            btn.classList.add(`btn${k}`)
+
+            liAdress.classList.add(`address${k}`)
+            liPhone.classList.add(`phone${k}`)
+            liWebsite.classList.add(`website${k}`)
+            liCompany.classList.add(`company${k}`)
+
+            bt[k]=document.querySelector(`.btn${k}`)
+
+            address[k]=document.querySelector(`.address${k}`)
+            displayNone(address[k])
+            
+            phone[k]=document.querySelector(`.phone${k}`)
+            displayNone(phone[k])
+
+            website[k]=document.querySelector(`.website${k}`)
+            displayNone(website[k])
+
+            company[k]=document.querySelector(`.company${k}`)
+            displayNone(company[k])
+
+            bt[k].addEventListener('click',function(){
+                console.log('mandeha');
+                
+        
+                if(bt[k].innerHTML=='<i class="fa-regular fa-eye"></i>' ){
+                    bt[k].innerHTML='<i class="fa-regular fa-eye-slash"></i>'
+                    displayBlock(address[k])
+                    displayBlock(phone[k])
+                    displayBlock(website[k])
+                    displayBlock(company[k])
+                }
+                else if(bt[k].innerHTML=='<i class="fa-regular fa-eye-slash"></i>' ){
+                    bt[k].innerHTML='<i class="fa-regular fa-eye"></i>'
+                    displayNone(address[k])
+                    displayNone(phone[k])
+                    displayNone(website[k])
+                    displayNone(company[k])
+                }
+                
+            
+                
+        })
+        
+            img.src = "./assets/img/user-avatar.png"
+            liName.innerHTML =    '<span class="ok">Name</span>'+'<span>: </span>'+ obj.name
+            liUserName.innerHTML ='<span class="ok">UserName</span> '+'<span>: </span>' +obj.username
+            liEmail.innerHTML =   '<span class="ok">Email</span>'+'<span>: </span>' +  obj.email
+            liAdress.innerHTML =  '<span class="ok">Address</span>'+'<span>: </span>' + obj.address
+            liPhone.innerHTML =   '<span class="ok">Phone</span>'+'<span>: </span>' + obj.phone
+            liWebsite.innerHTML = '<span class="ok">Website</span>'+'<span>: </span>' + obj.website
+            liCompany.innerHTML = '<span class="ok">Company</span>'+'<span>: </span>' + obj.company
+
+            
+            address.forEach(add => {
+                enfant =add.querySelector('.ok')
+                enfant.style.marginRight="27px"
+                
+            });
+            phone.forEach(add => {
+                enfant =add.querySelector('.ok')
+                enfant.style.marginRight="41px"
+                
+            });
+            website.forEach(add => {
+                enfant =add.querySelector('.ok')
+                enfant.style.marginRight="27px"
+                
+            });
+            company.forEach(add => {
+                enfant =add.querySelector('.ok')
+                enfant.style.marginRight="10px"
+                
+            });
+        }
+    });
+    
+    let loader = document.querySelector('.loader').remove()
+}, 2000);
+
